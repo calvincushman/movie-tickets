f93f457fea1939fce5d4bc74abe6e9767761ae47
Calvin Cushman and David Rolfs <calvin@>
2017-01-24 10:37:33 -0800

    add ticket constructor business and user logic

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function Ticket(age, time, movie) {
+  this.age = age;
+  this.time = time;
+  this.movie = movie;
+}
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+    var inputtedAge = $(this).find("input#age").val();
+    var inputtedTime = $(this).find("input.time").val();
+    var inputtedMovie = $(this).find("input#movieTitles").val();
+
+
+});
